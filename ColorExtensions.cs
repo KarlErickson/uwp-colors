7f48a453257bb06ce909f59f314ec9a222b34e6e
Karl Erickson <karler@microsoft.com>
2020-08-16 07:23:56 -0700

    code

---
+++ ColorExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Windows.UI;
+
+public static class ColorExtensions
+{
+    public static string GetName(this Color color) =>
+        ColorNameTuples
+        .Where(c => c.Item1.Equals(color))
+        .Select(c => c.Item2).FirstOrDefault();
+
+    public static Color GetContrastingForegroundColor(this Color c) =>
+        c.R * 0.3 + c.G * 0.59 + c.B * 0.11 > 127 ? Colors.Black : Colors.White;
+
+    private static List<Tuple<Color, string>> ColorNameTuples =>
+        typeof(Colors).GetRuntimeProperties()
+        .Select(p => Tuple.Create((Color)p.GetValue(null), p.Name))
+        .ToList();
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Windows.UI;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+
+namespace UwpColors
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        List<NamedColor> NamedColors { get; } =
+            (from property in typeof(Colors).GetRuntimeProperties()
+             let color = (Color)property.GetValue(null)
+             select new NamedColor
+             {
+                 Brush = new SolidColorBrush(color),
+                 Name = color.GetName(),
+                 ForegroundBrush = new SolidColorBrush(color.GetContrastingForegroundColor())
+             })
+            .ToList();
+    }
+
+    public class NamedColor
+    {
+        public Brush Brush { get; set; }
+        public string Name { get; set; }
+        public Brush ForegroundBrush { get; set; }
+    }
+}
